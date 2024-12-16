666cf1fe83ef56557862a638a223f45f3d3fc14c
rankrev <307532500@qq.com>
2024-12-16 17:49:39 +0800

    重新格式化行

--- src/ParallelTransformAction.cpp
+++ src/ParallelTransformAction.cpp
@@ -16,8 +16,7 @@
 using namespace clang;
 using namespace ast_matchers;
 
-const ast_matchers::internal::VariadicDynCastAllOfMatcher<Stmt,
-                                                                 AttributedStmt>
+const ast_matchers::internal::VariadicDynCastAllOfMatcher<Stmt, AttributedStmt>
     attributedStmt;
 
 static StatementMatcher buildForRangeMatcher() {
