c6771c83f71e98cceb787ea18638791ac69c3880
rankrev <307532500@qq.com>
2024-12-19 21:23:16 +0800

    添加transformer可执行程序来进行transform

---
+++ transformer/ParallelAttribute.cpp
@@ -0,0 +1,54 @@
+#include "clang/AST/ASTContext.h"
+#include "clang/AST/Attr.h"
+#include "clang/AST/Attrs.inc"
+#include "clang/AST/Stmt.h"
+#include "clang/Basic/Diagnostic.h"
+#include "clang/Basic/DiagnosticSema.h"
+#include "clang/Basic/ParsedAttrInfo.h"
+#include "clang/Sema/ParsedAttr.h"
+#include "clang/Sema/Sema.h"
+using namespace clang;
+
+namespace {
+struct ParallelAttrInfo : public ParsedAttrInfo {
+  ParallelAttrInfo() {
+    OptArgs = 15;
+    static constexpr Spelling spellings[] = {
+        {ParsedAttr::AS_CXX11, "parallel"},
+    };
+    Spellings = spellings;
+  }
+
+  bool diagAppertainsToDecl(Sema &S, const ParsedAttr &Attr,
+                            const Decl *D) const override {
+    S.Diag(Attr.getLoc(), diag::err_attribute_wrong_decl_type_str)
+        << Attr << Attr.isRegularKeywordAttribute()
+        << "for-range(C++11) loop statements";
+    return false;
+  }
+
+  bool diagAppertainsToStmt(Sema &S, const ParsedAttr &Attr,
+                            const Stmt *St) const override {
+    if (!isa<CXXForRangeStmt>(St)) {
+      S.Diag(Attr.getLoc(), diag::err_attribute_wrong_decl_type_str)
+          << Attr << Attr.isRegularKeywordAttribute()
+          << "for-range(C++11) loop statements";
+      static auto id =
+          S.Diags.getCustomDiagID(DiagnosticsEngine::Note, "not '%0'");
+      S.Diag(St->getBeginLoc(), id) << St->getStmtClassName();
+      return false;
+    }
+    return true;
+  }
+
+  AttrHandling handleStmtAttribute(Sema &S, Stmt *St, const ParsedAttr &Attr,
+                                   class Attr *&Result) const override {
+    Result = AnnotateAttr::Create(S.Context, "parallel", nullptr, 0,
+                                  Attr.getRange());
+    return AttributeApplied;
+  }
+};
+} // namespace
+
+static ParsedAttrInfoRegistry::Add<ParallelAttrInfo>
+    X("parallel", "parallel attribute for loop");
---
+++ transformer/ParallelTransformer.cpp
@@ -0,0 +1,123 @@
+#include "clang/ASTMatchers/ASTMatchFinder.h"
+#include "clang/ASTMatchers/ASTMatchers.h"
+#include "clang/Basic/ParsedAttrInfo.h"
+#include "clang/Frontend/FrontendActions.h"
+#include "clang/Tooling/CommonOptionsParser.h"
+#include "clang/Tooling/Refactoring/AtomicChange.h"
+#include "clang/Tooling/Tooling.h"
+#include "clang/Tooling/Transformer/RangeSelector.h"
+#include "clang/Tooling/Transformer/RewriteRule.h"
+#include "clang/Tooling/Transformer/Stencil.h"
+#include "clang/Tooling/Transformer/Transformer.h"
+#include "llvm/ADT/ArrayRef.h"
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/Error.h"
+#include "llvm/Support/raw_ostream.h"
+#include <memory>
+
+namespace {
+using namespace clang;
+AST_MATCHER_P(AttributedStmt, hasParallelAttribute,
+              ast_matchers::internal::Matcher<Stmt>, InnerMatcher) {
+  for (const auto *attr : Node.getAttrs()) {
+    if (!isa<AnnotateAttr>(attr))
+      continue;
+    const auto *annotateAttr = cast<AnnotateAttr>(attr);
+    if (annotateAttr->getAnnotation() == "parallel") {
+      // llvm::errs() << "Found parallel attribute\n";
+      // Node.getSubStmt()->dump();
+      return InnerMatcher.matches(*Node.getSubStmt(), Finder, Builder);
+    }
+  }
+  return false;
+}
+} // namespace
+
+using namespace llvm;
+using namespace clang;
+using namespace tooling;
+using namespace ast_matchers;
+using transformer::addInclude;
+using transformer::cat;
+using transformer::changeTo;
+using transformer::IncludeFormat;
+using transformer::makeRule;
+using transformer::node;
+
+const ast_matchers::internal::VariadicDynCastAllOfMatcher<Stmt, AttributedStmt>
+    attributedStmt;
+
+static cl::OptionCategory
+    ParallelTransformCategory("parallel-transform options");
+
+static StatementMatcher buildMatcher() {
+  return attributedStmt(
+             hasParallelAttribute(
+                 cxxForRangeStmt(hasBody(compoundStmt().bind("body")),
+                                 hasLoopVariable(varDecl().bind("var")),
+                                 hasRangeInit(expr().bind("range")))
+                     .bind("for")))
+      .bind("attr");
+}
+
+bool applySourceChanges(const AtomicChanges &Changes) {
+  std::set<std::string> Files;
+  for (const auto &Change : Changes)
+    Files.insert(Change.getFilePath());
+  tooling::ApplyChangesSpec Spec;
+  Spec.Cleanup = false;
+  for (const auto &File : Files) {
+    llvm::ErrorOr<std::unique_ptr<llvm::MemoryBuffer>> BufferErr =
+        llvm::MemoryBuffer::getFile(File);
+    if (!BufferErr) {
+      llvm::errs() << "error: failed to open " << File << " for rewriting\n";
+      return true;
+    }
+    auto Result = tooling::applyAtomicChanges(File, (*BufferErr)->getBuffer(),
+                                              Changes, Spec);
+    if (!Result) {
+      llvm::errs() << toString(Result.takeError());
+      return true;
+    }
+    llvm::errs() << *Result;
+  }
+  return false;
+}
+
+int main(int argc, const char **argv) {
+  auto ExpectedParser =
+      CommonOptionsParser::create(argc, argv, ParallelTransformCategory);
+  if (!ExpectedParser) {
+    llvm::errs() << ExpectedParser.takeError();
+    return 1;
+  }
+  CommonOptionsParser &OptionsParser = ExpectedParser.get();
+  ClangTool Tool(OptionsParser.getCompilations(),
+                 OptionsParser.getSourcePathList());
+
+  AtomicChanges Changes;
+  auto Consumer = [&](Expected<MutableArrayRef<AtomicChange>> C) {
+    if (C) {
+      Changes.insert(Changes.end(), std::make_move_iterator(C->begin()),
+                     std::make_move_iterator(C->end()));
+    } else {
+      llvm::errs() << "Error generating changes: "
+                   << llvm::toString(C.takeError()) << "\n";
+    }
+  };
+
+  auto Rule = makeRule(
+      buildMatcher(),
+      {addInclude("algorithm", IncludeFormat::Angled),
+       changeTo(cat("std::for_each(std::execution::par, ", "std::begin(",
+                    node("range"), "), ", "std::end(", node("range"), "), ",
+                    "[&](", node("var"), ")", node("body"), ");"))});
+  Transformer Transformer(std::move(Rule), std::move(Consumer));
+  MatchFinder Finder;
+  Transformer.registerMatchers(&Finder);
+
+  Tool.run(newFrontendActionFactory(&Finder).get());
+
+  applySourceChanges(Changes);
+  return 0;
+}
